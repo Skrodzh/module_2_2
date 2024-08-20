f628e1139166a46627777bd1324f553003180a7d
Skrodzh <zenit.nadir@mail.ru>
2024-08-20 22:12:16 +0300

    Add files via upload

---
+++ Module_2_2.py
@@ -0,0 +1,9 @@
+a = int(input('First: '))
+b = int(input('Second: '))
+c = int(input('Therd: '))
+if a == b != c or b == c != a or c == a != b:
+    print(2)
+elif a == b == c:
+    print(3)
+elif a != b !=c:
+    print(0)
